746aeefcd31a4d0327eb8b6da3757c26b977d63c
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-07-16 11:38:20 +0500

    added EducationInfo component with basic styling and also added EducationInfo.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import GeneralInfo from "./components/GeneralInfo";
+import EducationInfo from "./components/EducationInfo";
 
 import { useState } from "react";
 
 export default function App(){
+
+  //  useStates for GeneralInfo
   const [generalInfo,setGeneralInfo]=useState(
     {
       fullName:'',
@@ -14,6 +17,17 @@ export default function App(){
 
   const [isEditingGeneral,setIsEditingGeneral]=useState(true);
 
+
+  // useState  for  EduationInfo
+  const [educationInfo,setEducationInfo]=useState({
+    schoolName:'',
+    titleOfStudy:'',
+    dateOfStudy:''
+
+  })
+
+  const [isEditingEducation,setIsEditingEducation]=useState(true);
+
    return(
     <div  className="app">
 
@@ -22,9 +36,14 @@ export default function App(){
     setGeneralInfo={setGeneralInfo}  
     isEditingGeneral={isEditingGeneral}  
     setIsEditingGeneral={setIsEditingGeneral}
-      
     />
 
+    <EducationInfo
+      educationInfo={educationInfo}
+      setEducationInfo={setEducationInfo}
+      isEditingEducation={isEditingEducation}
+      setIsEditingEducation={setIsEditingEducation}
+    />
     </div>
 
    )
---
+++ src/components/EducationInfo.jsx
@@ -0,0 +1,43 @@
+import '../styles/EducationInfo.css'
+
+export default function EducationInfo({educationInfo,setEducationInfo,isEditingEducation,setIsEditingEducation}){
+
+
+    function handleChange(e){
+     const {name,value}=e.target;
+
+     setEducationInfo((prev)=>({...prev,[name]:value}))
+    }
+
+    return(
+        <div className='education-info'>
+            <h2>Education Info</h2>
+            {
+                (isEditingEducation)?(
+                    <div className='education-info-content'>
+                        <label        htmlFor='school-name'          >School Name</label>
+                        <input   type='text'  id='school-name'  name= 'schoolName'  value={educationInfo.schoolName}  onChange={handleChange}/>
+                        <label     htmlFor='title-of-study'      >Title Of Study</label>
+                        <input   type='text' id='title-of-study' name='titleOfStudy'    value={educationInfo.titleOfStudy}  onChange={handleChange} />
+                        <label      htmlFor='date-of-study'         >Date of Study</label>
+                        <input   type='date' id='date-of-study'   name='dateOfStudy'  value={educationInfo.dateOfStudy} onChange={handleChange}/>
+                        <button onClick={()=>setIsEditingEducation(false)}>Submit</button>
+
+                    </div>
+
+                ):
+                (
+                   <div className='education-info-preview'>
+                    <p> <strong>SchoolName:</strong>{educationInfo.schoolName}</p>
+                    <p> <strong>TitleOfStudy:</strong>{educationInfo.titleOfStudy}</p>
+                    <p> <strong>DateOfStudy</strong>{educationInfo.dateOfStudy}</p>
+                    <button  onClick={()=>setIsEditingEducation(true)}>Edit</button>
+                   </div>
+                )
+
+            }
+        </div>
+    )
+
+
+}
