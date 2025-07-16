83045a8f62af407fdbc43e9cc90354af8594e399
Nimra Jabbar <nimraabduljabbar14@gmail.com>
2025-07-16 10:02:34 +0500

    added GeneralInfo component for storing user information and displaying  with basic styling and aslo added  GeneralInfo.css file

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# React + Vite
+## General Info 
+- enter  name,email ,phone number in input fields with submit button
+- display of  name ,email and phone   with edit  btn 
+-toggle between  edit mode  and display mode
+-use state  from react to  track user info (name,eamil,phone) and tracking mode  between edit or display
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,48 @@
+import '../styles/GeneralInfo.css';
+
+export default function GeneralInfo({generalInfo,setGeneralInfo,isEditingGeneral,setIsEditingGeneral}){
+
+    function handleChange(e){
+        const {name,value}=e.target;
+        setGeneralInfo((prev)=>({...prev,[name]:value.trim()}));
+
+    }
+
+    
+ return (
+   <div  className="general-info">
+
+    <h2>General Information</h2>
+
+    { (isEditingGeneral)?(
+
+    <div  className="general-info-content">
+
+      <label htmlFor='user-name'>Name</label>
+      <input type='text' id='user-name'    name='fullName' value={generalInfo.fullName}   onChange={handleChange}/>
+      <label  htmlFor='user-email'>Email</label>
+      <input type='email' id='user-email'   name='email' value={generalInfo.email}   onChange={handleChange} />
+      <label  htmlFor='user-phone'>Phone</label>
+      <input  type='number' id='user-phone' name='phone' value={generalInfo.phone}   onChange={handleChange}/>
+      <button    onClick={()=>setIsEditingGeneral(false)}> Submit</button>
+
+      </div>
+
+     ):(
+       <div className="general-info-preview">
+       <p><strong>Name:</strong>{generalInfo.fullName}</p>
+       <p><strong>Email:</strong>{generalInfo.email}</p>
+       <p><strong>Phone:</strong>{generalInfo.phone}</p>
+       <button  onClick={()=> setIsEditingGeneral(true)}>Edit</button>
+       </div>
+      
+
+     )
+      
+     
+    }
+
+   </div>
+ )
+
+}
